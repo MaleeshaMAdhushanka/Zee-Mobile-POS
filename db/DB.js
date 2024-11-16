55e24cbb23cd609bd805567df56ddab5173d5cc7
MaleeshaMAdhushanka <zeejmaleeshamadushanka@gmail.com>
2024-11-16 14:38:33 +0530

    Done fixing errors and Pages

---
+++ db/DB.js
@@ -0,0 +1,30 @@
+export let Customers = [
+    {
+        custId : 'C01',
+        custName : 'Wanindu',
+        custAddress : 'Colombo',
+        custSalary : '20000'
+    } ,
+
+    {
+        custId : 'C02',
+        custName : 'Gayshan',
+        custAddress : 'Kandy',
+        custSalary : '10000'
+    }
+];
+export let Items = [
+    {
+        itemId : 'I01',
+        itemName : 'Iphone 15',
+        itemQty : 100,
+        itemPrice : 160000
+    },
+    {
+        itemId : 'I02',
+        itemName : 'IPhone 11',
+        itemQty : 50,
+        itemPrice : 120000
+    }
+];
+export let Orders = [];
